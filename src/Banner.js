ba0c61db79dbb2dcc0a84c8ae275e56acafd9cf6
kavya s <kavya.s@thoughtclan.com>
2023-08-17 18:30:49 +0530

    responsive added

--- src/Banner.js
+++ src/Banner.js
@@ -42,8 +42,8 @@ function Banner() {
             <button className="banner__button">Play</button>
             <button className="banner__button">My List</button>
         </div>
-        <h1 className="banner__desriptioon"> {movie?.overview}</h1>
-        {truncate(movie?.overview, 150)}
+        <h1 className="banner__description">{truncate(movie?.overview, 90)} </h1>
+        
         </div>
         <div className="banner__fadeBottom" />
       </header>
